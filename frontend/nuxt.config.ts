c0b6d5a26cdc8d2c655b3addba41f7731a32459b
alt-potato <jason.why.zhuang@gmail.com>
2026-03-09 17:02:34 -0400

    Add debug proxy config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,4 +2,29 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
+  nitro: {
+    devProxy: {
+      "/action": {
+        target: "http://localhost:8080/action",
+        changeOrigin: true,
+        ws: false,
+      },
+      "/events": {
+        target: "http://localhost:8080/events",
+        changeOrigin: true,
+        ws: false,
+      },
+      "/state": {
+        target: "http://localhost:8080/state",
+        changeOrigin: true,
+        ws: false,
+      },
+      "/assets": {
+        target: "http://localhost:8080/assets",
+        changeOrigin: true,
+        ws: false,
+      },
+    },
+  },
 });
