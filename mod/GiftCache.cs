980362d708b21206946ba9df709f4edaf0f4d6d4
alt-potato <jason.why.zhuang@gmail.com>
2026-04-09 16:49:40 -0400

    Log gift sprite ppu and bounds for position debugging

--- mod/GiftCache.cs
+++ mod/GiftCache.cs
@@ -106,20 +106,7 @@ namespace PlayLoRWithMe
                             worldOffset += prefab.transform.localPosition;
                         }
 
-                        // Log hierarchy details for debugging position issues.
-                        if (frontRenderer != null)
-                        {
-                            var sb = new System.Text.StringBuilder();
-                            sb.Append($"gift {xml.id} ({xml.Name}) pos={xml.Position} offset=({worldOffset.x:F3},{worldOffset.y:F3}) hierarchy:");
-                            var dt = frontRenderer.transform;
-                            while (dt != null)
-                            {
-                                sb.Append($" [{dt.name} lp=({dt.localPosition.x:F3},{dt.localPosition.y:F3})]");
-                                if (dt == prefab.transform) break;
-                                dt = dt.parent;
-                            }
-                            Debug.Log($"[PlayLoRWithMe] GiftCache: {sb}");
-                        }
+                        Debug.Log($"[PlayLoRWithMe] GiftCache: gift {xml.id} ({xml.Name}) pos={xml.Position} offset=({worldOffset.x:F3},{worldOffset.y:F3}) ppu={sprite.pixelsPerUnit:F1} bounds=({sprite.bounds.min.x:F3},{sprite.bounds.min.y:F3})-({sprite.bounds.max.x:F3},{sprite.bounds.max.y:F3}) texRect=({sprite.textureRect.width}x{sprite.textureRect.height})");
 
                         File.WriteAllBytes(path,
                             AppearanceCache.SpriteToPng(sprite, canvasW, canvasH, bounds, ppu, worldOffset));
